81a2970f378c4fbcd291867ca9daba6698dfe086
Eric Zorn <ericzorndesigns@gmail.com>
2018-06-25 12:26:17 -0400

    Fix Album Model For Belongs_To Required and View Logic

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,6 +2,6 @@ class Album < ApplicationRecord
   # belongs_to :record_label
   # validates :record_label_id, presence: true
 
-  belongs_to :record_label, required: false
+  belongs_to :record_label, required: true
   validates :record_label_id, presence: true
 end
