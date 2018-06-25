10cc4de1e14efe9f96e30f5189e18c2fe82037da
Eric Zorn <ericzorndesigns@gmail.com>
2018-06-25 19:09:12 -0400

    Logic Model Updates

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,4 +2,8 @@ class Album < ApplicationRecord
   belongs_to :record_label, required: true
   validates :record_label_id, presence: true
   validates :year, presence: true, length: {maximum: 4, minimum: 4}
+  validates :band, presence: true, length: {maximum: 250, minimum: 3}
+  validates :title, presence: true, length: {maximum: 250, minimum: 3}
+  validates :genre, presence: true, length: {maximum: 250, minimum: 3}
+  validates :plays, presence: true, length: {maximum: 10, minimum: 1}
 end
