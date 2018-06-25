f9d94e4e6747f005a1ae79c29264d39c2fb02241
Eric Zorn <ericzorndesigns@gmail.com>
2018-06-25 16:43:48 -0400

    Record Label Model Validations

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,7 +1,5 @@
 class Album < ApplicationRecord
-  # belongs_to :record_label
-  # validates :record_label_id, presence: true
-
   belongs_to :record_label, required: true
   validates :record_label_id, presence: true
+  validates :year, presence: true, length: {maximum: 4, minimum: 4}
 end
--- app/models/record_label.rb
+++ app/models/record_label.rb
@@ -1,3 +1,5 @@
 class RecordLabel < ApplicationRecord
   has_many :albums, dependent: :destroy
+
+  validates :name, length: {minimum: 5, maximum: 50}
 end
