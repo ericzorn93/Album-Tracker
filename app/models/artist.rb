5c2a06fd5a1968820b67fe0203074870cfd71954
Eric Zorn <ericzorndesigns@gmail.com>
2018-07-02 17:24:45 -0400

    Validating Prescence in Artist Model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,6 +2,6 @@ class Artist < ApplicationRecord
   has_and_belongs_to_many :albums, dependent: :destroy
 
   # Validations
-  validates :name, length: { minimum: 3, maximum: 255}
-  validates :genre, length: { minimum: 3, maximum: 255}
+  validates :name, length: { minimum: 3, maximum: 255}, presence: true
+  validates :genre, length: { minimum: 3, maximum: 255}, presence: true
 end
