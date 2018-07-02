99912241e77819e7d092b6f01957d78b3551bc86
Eric Zorn <ericzorndesigns@gmail.com>
2018-07-02 17:23:56 -0400

    Add Artist Model Validations

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,7 @@
 class Artist < ApplicationRecord
   has_and_belongs_to_many :albums, dependent: :destroy
+
+  # Validations
+  validates :name, length: { minimum: 3, maximum: 255}
+  validates :genre, length: { minimum: 3, maximum: 255}
 end
