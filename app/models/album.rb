2a0b3fc6f9d4bc7536ce99489c144dfdb9200f11
Eric Zorn <ericzorndesigns@gmail.com>
2018-07-02 16:05:21 -0400

    Updating Band Display and Artist Edit

--- app/models/album.rb
+++ app/models/album.rb
@@ -5,7 +5,7 @@ class Album < ApplicationRecord
   validates :artist_ids, presence: true
   validates :record_label_id, presence: true
   validates :year, presence: true, length: {maximum: 4, minimum: 4}
-  # validates :band, presence: true, length: {maximum: 250, minimum: 3}
+  validates :band, presence: true, length: {maximum: 250, minimum: 3}
   validates :title, presence: true, length: {maximum: 250, minimum: 3}
   validates :genre, presence: true, length: {maximum: 250, minimum: 3}
   validates :plays, presence: true, length: {maximum: 10, minimum: 1}
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,3 @@
 class Artist < ApplicationRecord
-  has_and_belongs_to_many :albums
+  has_and_belongs_to_many :albums, dependent: :destroy
 end
