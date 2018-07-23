112a7704f349566fe47ffc89bc517e9a91f7b8d8
Eric Zorn <ericzorndesigns@gmail.com>
2018-07-23 12:44:32 -0400

    Contact Scaffold Validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,8 @@
 class Contact < ApplicationRecord
+  # Validations
+  validates :first_name, presence: true, length: { minimum: 2, maximum: 250 }
+  validates :last_name, presence: true, length: { minimum: 2, maximum: 250 }
+  validates :phone_number, presence: true, length: { minimum: 10, maximum: 100 }
+  validates :email, presence: true, length: { minimum: 5, maximum: 250 }
+  validates :message, presence: true, length: { minimum: 5, maximum: 800 }
 end
